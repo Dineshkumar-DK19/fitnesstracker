bad34b40a8cb952b2eb53997af83099dd091e833
Dineshkumar <dineshmageswaran1918@gmail.com>
2026-02-13 15:44:07 +0530

    profile module done

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/home", require("./routes/homeRoutes"));
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/user", require("./routes/userRoutes"));
-app.use("/api/workouts", require("./routes/workoutRoutes"));
-app.use("/api/dashboard", require("./routes/dashboardRoutes"));
-
+app.use("/api/home", require("./src/routes/homeRoutes"));
+app.use("/api/auth", require("./src/routes/authRoutes"));
+app.use("/api/user", require("./src/routes/userRoutes"));
+app.use("/api/workouts", require("./src/routes/workoutRoutes"));
+app.use("/api/dashboard", require("./src/routes/dashboardRoutes"));
 module.exports = app;
